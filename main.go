280fe9cd26bb2a26a1a36eeecd3cc14c2e7a9695
jang294 <98522102+jang294@users.noreply.github.com>
2023-11-08 12:07:05 +0900

    Update main.go

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", index)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
