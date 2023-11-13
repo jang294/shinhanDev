a746138c5f4916d28e2350da3a6352b2c37ceaf4
jang294 <98522102+jang294@users.noreply.github.com>
2023-11-13 16:17:38 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, S2D service Demo ! - v3.0")
+	fmt.Fprintf(w, "Hello, S2D service Demo ! - v4.0")
 }
 
 func main() {
